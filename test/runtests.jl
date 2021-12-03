3232483bd40349cdcc7bbaf4b4f36970fcd5770b
Josh <59419126+this-josh@users.noreply.github.com>
2021-12-03 15:19:52 +0000

    fix expected values

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,9 @@ using AdventOfCode2021
 using Test
 
 @testset "Day 1" begin
-    @test AdventOfCode2021.Day01.day01() == [1527, 1575]
+    @test AdventOfCode2021.Day01.day01() == [1655, 1683]
 end
 
 @testset "Day 2" begin
-    @test AdventOfCode2021.Day02.day02() == [1714680, 1963088820]
+    @test AdventOfCode2021.Day02.day02() == [2019945, 1599311480]
 end
