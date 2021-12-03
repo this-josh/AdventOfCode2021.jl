b8c22f8aed0b6bcfa9d988e84b2fa3eb7e2c636f
Josh <59419126+this-josh@users.noreply.github.com>
2021-12-03 15:22:37 +0000

    fix parens

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,9 @@ using AdventOfCode2021
 using Test
 
 @testset "Day 1" begin
-    @test AdventOfCode2021.Day01.day01() == [1655, 1683]
+    @test AdventOfCode2021.Day01.day01() == (1655, 1683)
 end
 
 @testset "Day 2" begin
-    @test AdventOfCode2021.Day02.day02() == [2019945, 1599311480]
+    @test AdventOfCode2021.Day02.day02() == (2019945, 1599311480)
 end
