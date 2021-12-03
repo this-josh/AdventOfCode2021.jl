7f7ab7bb31de1673a2f4c00cb0cbfc7d743c8321
Josh <59419126+this-josh@users.noreply.github.com>
2021-12-03 14:17:42 +0000

    trying to add my methods

--- src/day01.jl
+++ src/day01.jl
@@ -1,11 +1,23 @@
 module Day01
 
 using AdventOfCode2021
+using IterTools
 
-function day01(input::String = readInput(joinpath(@__DIR__, "..", "data", "day01.txt")))
-    reports = parse.(Int, split(input))
-    sums = [a + b + c for (a, b, c) ∈ zip(reports[1:end-2], reports[2:end-1], reports[3:end])]
-    return [count(diff(reports) .> 0), count(diff(sums) .> 0)]
+function num_increasing(arr)
+    arr_shift = arr[2:end]
+    diff = arr[1:end-1]-arr_shift
+    return count(ii->(ii<0), diff)
+end
+
+function day01(data::String = readInput(joinpath(@__DIR__, "..", "data", "day01.txt")))
+    # part 1
+    part1 = num_increasing(data)
+    # part 2
+    windows = zeros(size(data)[1])
+    for (idx, window) in enumerate(IterTools.partition(data, 3,1))
+        windows[idx]=  sum(window)
+    
+    return part1, windows
 end
 
 end # module
--- src/day02.jl
+++ src/day02.jl
@@ -2,23 +2,41 @@ module Day02
 
 using AdventOfCode2021
 
-function day02(input::String = readInput(joinpath(@__DIR__, "..", "data", "day02.txt")))
-    vertical, horizontal, vertical_p2, aim = 0, 0, 0, 0
-    for line in split(rstrip(input), '\n')
-        dir, x = split(line)
-        x = parse(Int, x)
-        if dir == "forward"
-            horizontal += x
-            vertical_p2 += aim * x
-        elseif dir == "up"
-            vertical -= x
-            aim -= x
-        elseif dir == "down"
-            vertical += x
-            aim += x
+function day02(data::String = readInput(joinpath(@__DIR__, "..", "data", "day02.txt")))
+    # part 1
+    horizontal = 0
+    depth = 0 
+    for (command, value) in eachrow(data)
+        if command == "forward"
+            horizontal+=value
+        elseif command == "down"
+            depth+=value
+        elseif command == "up"
+            depth-=value
+        else
+            @show command
+            throw(KeyError(command))
         end
     end
-    return [vertical * horizontal, vertical_p2 * horizontal]
-end
+    part1 = depth*horizontal
 
+    # part 2
+    horizontal = 0
+    aim = 0 
+    depth = 0
+    for (command, value) in eachrow(data)
+        if command == "forward"
+            horizontal+=value
+            depth+=aim*value
+        elseif command == "down"
+            aim+=value
+        elseif command == "up"
+            aim-=value
+        else
+            @show command
+            throw(KeyError(command))
+        end
+    end
+    part2 = depth*horizontal
+    return part1,part2
 end # module
